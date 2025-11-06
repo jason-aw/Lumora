df1328c01e642e5640ec97b0159a6739e5694bb7
jason <jaw878@gmail.com>
2025-11-07 09:52:20 +1100

    Initial Commit

---
+++ Lumora/LumoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumoraApp.swift
+//  Lumora
+//
+//  Created by Jason Andersen Winfrey on 7/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct LumoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
